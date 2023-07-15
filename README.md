f256223daf4ff464a3f4391a26913d5de5827101
DaviiJones <davonbridgett@pursuit.org>
2023-07-15 10:26:21 -0400

    just about good to go.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 Find all sorts of information and nutritional facts about various fruits by typing the name into the search bar!
 
+<a href="index.html">Link to Landing Page! </a>
+
--- index.js
+++ index.js
@@ -1,49 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const URL = "https://www.fruityvice.com/api/fruit";
+  const button = document.querySelector("button");
+  const input = document.querySelector("input");
+  const fruitName = document.getElementById("fruitName");
+  const result = document.getElementById("result");
 
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = input.value;
 
+    fetch(`${URL}/${id}`)
+      .then((response) => response.json())
+      .then((json) => {
+        debugger;
+        result.innerHTML = `<h3>${json.name}</h3>
+        <li>Family: ${json.family}</li>
+        <li>Order: ${json.order}</li>
+        <li>Calories: ${json.nutritions.calories}</li>
+        <li>Fat: ${json.nutritions.fat}</li>
+        <li>Sugar: ${json.nutritions.sugar}</li>
+        <li>Carbohydrates: ${json.nutritions.carbohydrates}</li>
+        <li>Protein: ${json.nutritions.protein}</li>
+       `;
+        showfruit(json);
+      })
+      .catch((err) => showError(err));
+  });
 
-
-const URL = "https://www.fruityvice.com/api/fruit";
-const button = document.querySelector("button");
-const input = document.querySelector("input");
-const fruitName = document.getElementById("fruitName"); // Assuming "fruitName" is the id of the target HTML element
-const result = document.getElementById("result")
-
-
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  const id = input.value;
-  
-  fetch(`${URL}/${id}`)
-    .then((response) => response.json())
-    .then((json) => {
-        debugger
-        result.innerHTML =
-         `<h3>${json.name}</h3>
-         <li>${json.nutritions.calories}</li>`
-      showfruit(json);
-    })
-    .catch((err) => showError(err));
-
-    
-})
-
-
-
-/*const showfruit = (json) => {
-  fruitName.innerHTML = `
-    <article>
-        <h2>${json.name}</h2>
-    </article>
-`;
-};*/
-
-const showError = (err) => {
-  fruitName.innerHTML = `
+  const showError = (err) => {
+    fruitName.innerHTML = `
     <section class="error">
          <p>There was an error!</p>
          <p class="message">${err}</p>
      </section>
  `;
-};
-})
+  };
+});
